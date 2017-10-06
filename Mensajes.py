a1e864dafe76e98be42cd0dfc01aea48952c4cf5
Agonzga <32441374+Agonzga@users.noreply.github.com>
2017-10-06 17:57:09 -0500

    Update Mensajes.py

--- Mensajes.py
+++ Mensajes.py
@@ -1,1 +1,15 @@
+class Mensajes():
 
+	mensaje = {
+		"menu" : """
+			|---------------------------------------------------------------------------------------|
+			|------------------------------ Bienvenido a Modocur -----------------------------------|
+			|---------------------------------------------------------------------------------------|
+    		Operaciones
+    		1. Registrarse
+    		2. Ingresar
+    		3. Ingresar datos ficticios desde txt
+    		4. Salir
+    	""",
+    	"operation" : "Seleccione la operación deseada: "
+	}
