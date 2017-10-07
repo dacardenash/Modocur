ff0dbf2b6bb2d987c2e78911a251c0806891860b
Agonzga <32441374+Agonzga@users.noreply.github.com>
2017-10-07 06:08:00 -0500

    Menu

--- Mensajes.py
+++ Mensajes.py
@@ -2,37 +2,36 @@ class Mensajes():
   
 	mensaje = {
 		"menu_inicio" : """
-			----------------------------------------
+
     		Operaciones
     		1. Registrarse
     		2. Ingresar
     		3. Ingresar datos ficticios desde txt
     		4. Salir
-    		----------------------------------------
     	""",
     	"menu_instructor" : """
-    		----------------------------------------
-			OPERACIONES
+    		Operaciones
 			1. Crear curso
 			2. Modificar curso
 			4. Consultar cursos creados
 			3. Comentar
 			5. Agregar modulo
-			6. Promedio de progreso estudiantes
-			7. Cerrar curso
-			8. Mayor promedio de curso
-			9. Lista de estudiantes por curso
-			----------------------------------------
-    	""",
+			7. ⌂--Promedio de progreso estudiantes
+			8. ⌂--Cerrar curso   (Cierra el curso y calcula la nota final de cada estudiante)
+			9. ⌂--Mayor promedio de curso
+			10. ⌂-- Consultar curso con mayor N° estudiantes
+			11. ⌂-- Generar reporte fecha | curso | n° estudiantes | promedio
+			12. ⌂-- Porcentaje ganadores y perdedores de un curso 
+			13. ⌂-- Modulo con mayor numero comentarios
+			14. Cerrar sesion
+		""",
     	"menu_estudiante" : """
-    		----------------------------------------
-			OPERACIONES
+    		Operaciones
 			1. Ver curso disponibles
 			2. Inscribirse
 			3. Ver modulo
 			4. Comentar
 			5. Ver Progreso cursos realizados
-			----------------------------------------
     	""",
     	"operation" : "Seleccione la operación deseada: ",
     	"input_name" : "Ingrese nombre",
