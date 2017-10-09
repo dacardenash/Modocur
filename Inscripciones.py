4712133d942ada3edee47e09a36d8f79176acf6f
Agonzga <32441374+Agonzga@users.noreply.github.com>
2017-10-09 09:47:24 -0500

    Cursos

--- Inscripciones.py
+++ Inscripciones.py
@@ -2,14 +2,13 @@ from Estudiante import Estudiante
 
 class Inscripcion:
 
-	def __init__(self, fecha, nota, progreso, num_vistos, estudiante, curso):
-
-	    self._fecha = fecha
-	    self._nota = nota
-	    self._progreso = progreso
-	    self._num_vistos = num_vistos
-	    self._estudiante = estudiante
+	def __init__(self, curso, fecha, estudiante):
 	    self._curso = curso
+	    self._fecha = fecha
+	    self._nota = 0
+	    self._progreso = 0
+	    self._estudiante = estudiante
+	    
 
     
 	"""def inscribirse(fecha, nota, progreso, num_vistos, estudiante, curso)
