0f6ac954230b759724ce3f39fcfcf34ded79d4e5
Chimuelos <chacontrujillomanuel@gmail.com>
2017-10-09 04:32:45 +0200

    Inscripcion
    Desarrollo del metodo de inscripcion

--- Inscripciones.py
+++ Inscripciones.py
@@ -1,2 +1,16 @@
+from Estudiante import Estudiante
+
 class Inscripcion:
-	pass
+
+	def__init__(self, fecha, nota, progreso, num_vistos, estudiante, curso):
+
+	    self._fecha = fecha
+        self._nota = nota
+	    self._progreso = progreso
+	    self._num_vistos = num_vistos
+	    self._estudiante = estudiante
+	    self._curso = curso
+
+	def inscribirse(fecha, nota, progreso, num_vistos, estudiante, curso):
+
+		s
